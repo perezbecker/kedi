645882f6859c15f1b56d460e0eed54684ff2fef6
Danok <daniel@perezbecker.com>
2017-02-12 18:47:33 -0500

    adding twitter code

--- trump-twitter.py
+++ trump-twitter.py
@@ -3,4 +3,11 @@ import auth
 
 api = twitter.Api(consumer_key=auth.consumer_key, consumer_secret=auth.consumer_secret, access_token_key=auth.access_token_key, access_token_secret=auth.access_token_secret)
 
-print api.VerifyCredentials()
+# print api.VerifyCredentials()
+
+t = api.GetUserTimeline('realDonaldTrump', count=5)
+
+tweets = [i.AsDict() for i in t]
+
+for t in tweets:
+    print t['text']
