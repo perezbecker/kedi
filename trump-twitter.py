f461242d34bf6909815412ceb5f1982cd5ec0f0c
Danok <daniel@perezbecker.com>
2017-02-12 18:59:38 -0500

    adding twitter code

--- trump-twitter.py
+++ trump-twitter.py
@@ -1,5 +1,7 @@
 import twitter
 import auth
+import time
+from microdotphat import write_string, scroll, clear, show
 
 api = twitter.Api(consumer_key=auth.consumer_key, consumer_secret=auth.consumer_secret, access_token_key=auth.access_token_key, access_token_secret=auth.access_token_secret)
 
@@ -10,4 +12,9 @@ t = api.GetUserTimeline(screen_name='@realDonaldTrump', count=5)
 tweets = [i.AsDict() for i in t]
 
 for t in tweets:
-    print t['text']
+    clear()
+    write_string(t['text'])
+    scroll()
+    show()
+    time.sleep(0.01)
+    # print t['text']
