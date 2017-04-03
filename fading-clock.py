ecebdf0ef4fdc7aeced3328f064c5b6a15c92780
Daniel Perez-Becker <dperezbecker@yammmer-inc.com>
2017-04-02 22:31:23 -0400

    show bus times

---
+++ fading-clock.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+import math
+import time
+
+from microdotphat import clear, set_brightness, show, write_string, WIDTH, HEIGHT
+
+
+print("""Fading Text
+
+Uses the brightness control to fade between messages.
+""")
+
+speed = 5
+
+
+
+string = 0
+shown = True
+
+show()
+
+# Start time. Phase offset by math.pi/2
+start = time.time()
+
+while True:
+    # Fade the brightness in/out using a sine wave
+    b = (math.sin((time.time() - start) * speed) + 1) / 2
+    set_brightness(b)
+
+    # At minimum brightness, swap out the string for the next one
+    if b < 0.002 and shown:
+        clear()
+
+        t = datetime.datetime.now()
+        if t.second % 2 == 0:
+            set_decimal(2, 1)
+            set_decimal(4, 1)
+        else:
+            set_decimal(2, 0)
+            set_decimal(4, 0)
+        write_string(t.strftime('%H%M%S'), kerning=False)
+
+        show()
+        shown = False
+
+    # At maximum brightness, confirm the string has been shown
+    if b > 0.998:
+        shown = True
+
+    # Sleep a bit to save resources, this wont affect the fading speed
+    time.sleep(0.01)
