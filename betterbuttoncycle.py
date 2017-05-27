382a6d4c3d3d3c495c5317ff8e9135ff213298c3
Danok <daniel@perezbecker.com>
2017-05-26 22:17:05 -0400

    debugging bus

--- betterbuttoncycle.py
+++ betterbuttoncycle.py
@@ -249,7 +249,7 @@ if __name__ == '__main__':
     twittermessages='T-sync'
     daysTilSweep='S-sync'
 
-    NumberOfModules = 6
+    NumberOfModules = 5
 
     ButtonTrack = getButtonPresses()
     ButtonThread = Thread(target=ButtonTrack.run)
